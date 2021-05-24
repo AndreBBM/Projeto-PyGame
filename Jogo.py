fcd8badbf9391c48709a26aa67f3c06f3255b502
GiovanaCA <giovana.gca2002@gmail.com>
2021-05-24 17:14:53 -0300

    Mudando o personagem de lugar

--- Jogo.py
+++ Jogo.py
@@ -7,7 +7,7 @@ config = Setup.LoadConfig()
 pygame.init()
 
 # Criando a tela do jogo
-# Dimensões
+# Dimensões da tela
 altura = 400
 largura = 700
 WIDTH_cart= 50
@@ -15,18 +15,19 @@ WIDTH_bura= 100
 HEIGHT_cart= 50
 HEIGHT_bura= 200
 
+tela = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption('Olha o Carteiro!')
+
+# Posições dos elementos
 buraco_x="?"
 buraco_y="?"
 carteiro_y=300
-carteiro_x=0
+carteiro_x=175
 buraco_speed="?"
 carteiro_speedx=1
 carteiro_speedy="?"
 
-tela = pygame.display.set_mode((largura,altura))
-pygame.display.set_caption('Olha o Carteiro!')
-
-# Carregando imagens: (Só estruturando)
+# Carregando imagens: somente estruturando
 background = pygame.image.load('Imagens/8bitNY.jpg').convert() # Não precisa de transparência aqui
 background= pygame.transform.scale(background, (1000, 800))
 carteiro_img = pygame.image.load('Imagens/MailmanFemale.png').convert_alpha()
@@ -34,15 +35,17 @@ carteiro_img= pygame.transform.scale(carteiro_img, (WIDTH_cart, HEIGHT_cart))
 buracos_img = pygame.image.load('Imagens/buraco.png').convert_alpha()
 buracos_img = pygame.transform.scale(buracos_img, (WIDTH_bura, HEIGHT_bura))
 
-#são criados 2 fundos, um incial e outro logo após o primeiro, que aparece quando o primeiro sai da tela
+# São criados 2 fundos, um incial e outro logo após o primeiro, que aparece quando o primeiro sai da tela
 bg_e = 0
 bg_d = background.get_width()
 
 # Iniciando a estrutura 
 Start = True
 
+# Rodando o cenário
 clock = pygame.time.Clock()
 FramesPerSecond=24
+
 # Loop Principal!
 while Start:
     config = Setup.LoadConfig()
@@ -57,16 +60,18 @@ while Start:
         if event.type == QUIT:
             Start = False
 
-    #velocidade com que o fundo se mexe
+    # Velocidade com que o fundo se mexe
     bg_e -= 2.4
     bg_d -= 2.4
 
-    #quando os fundos saem completamente da tela eles voltam para o início para passarem de novo 
+    # Quando os fundos saem completamente da tela, eles voltam para o início para passarem novamente
     if bg_e < background.get_width() * -1:
         bg_e = background.get_width()
     if bg_d < background.get_width() * -1:
         bg_d = background.get_width()
+    
     # Velocidade do buraco no espaço    
+
 
     # Pra não sair da sala
     if carteiro_x > 700: #Tamanho máximo da imagem
@@ -74,16 +79,15 @@ while Start:
 
     tela.fill((176, 196, 222))
 
-    # Linha importante = Faz o jogo ficar sempre atualizando
+    # Linhas importantes = Fazem o jogo ficar sempre se atualizando
     tela.blit(carteiro_img, (carteiro_x, carteiro_y))
-    #Linha importante= Faz o jogo ficar sempre atualizando, junto com o fundo
-
     tela.blit(background, (bg_e, -370))
     tela.blit(background, (bg_d, -370))
     tela.blit(carteiro_img, (carteiro_x, carteiro_y))
 
     pygame.display.update()
 
+# Encerrando o PyGame
 pygame.quit()
 
 

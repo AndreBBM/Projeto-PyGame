28a99d82285c872bd55d72988140916df86a8e3b
GiovanaCA <giovana.gca2002@gmail.com>
2021-05-24 11:19:59 -0300

    adicionando imagem do buraco

--- Images.py
+++ Images.py
@@ -3,3 +3,4 @@ import pygame
 carteiro = pygame.image.load('MailmanMale.png.png').convert()
 carteira = pygame.image.load('MailmanFemale.png').convert()
 cidade = pygame.image.load('8bitNY.png').convert()
+buraco = pygame.image.load('buraco.png').convert()

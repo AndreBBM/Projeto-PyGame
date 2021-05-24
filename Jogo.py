e2eb28716f89399f6ec198e53de81d58ccdf8e54
brigidolivia <livia.outreach@gmail.com>
2021-05-24 10:42:37 -0300

    Estruturando onde entram imagens

--- Jogo.py
+++ Jogo.py
@@ -3,15 +3,27 @@ import pygame
 from pygame.constants import QUIT
 pygame.init()
 
+#Dimensões
 altura=500
 largura=700
+WIDTH_cart="?"
+WIDTH_bura="?"
+HEIGHT_cart= "?"
+HEIGHT_bura= "?"
 
 tela = pygame.display.set_mode((largura,altura))
 
 pygame.display.set_caption('Olha o Carteiro!')
 
-Start=True
+#Carregando imagens: (Só estruturando)
+background = pygame.image.load('').convert() #Não precisa de transparência aqui
+carteiro_img = pygame.image.load('').convert_alpha()
+carteiro_img= pygame.transform.scale(carteiro_img, (WIDTH_cart, HEIGHT_cart))
+buracos_img = pygame.image.load('').convert_alpha()
+buracos_img = pygame.transform.scale(buracos_img, (WIDTH_bura, HEIGHT_bura))
 
+
+Start=True
 #Loop Principal!
 while Start:
     tela.fill((176, 196, 222))

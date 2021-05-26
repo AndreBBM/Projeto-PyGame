c30fccaf23d7db4a41cca5e9a2b8590ee7501536
GiovanaCA <giovana.gca2002@gmail.com>
2021-05-26 14:53:26 -0300

    Arrumando

--- Images.py
+++ Images.py
@@ -1,10 +1,5 @@
 import pygame
 
-WIDTH = 480
-HEIGHT = 600
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Navinha')
-
 carteiro = pygame.image.load('Imagens/MailmanFemale.png').convert()
 carteira = pygame.image.load('Imagens/MailmanMale.png').convert()
 cidade = pygame.image.load('Imagens/8bitNY.jpg').convert()

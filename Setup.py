352d9bfb8a184b6222d22eaddf8203e281c425f8
GiovanaCA <giovana.gca2002@gmail.com>
2021-05-26 19:44:43 -0300

    Apagando

--- Setup.py
+++
@@ -1,9 +0,0 @@
-#Pega variveis do arquivo GameConfig.json
-import json
-
-def LoadConfig():
-    # read file
-    with open('GameConfig.json', 'r') as arquivo_com_configuracoes:
-        data=arquivo_com_configuracoes.read()
-    # parse file
-    return json.loads(data)

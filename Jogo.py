f935e89e94a4a846531524c2ca714df9e7934f93
GiovanaCA <giovana.gca2002@gmail.com>
2021-05-31 18:32:54 -0300

    Organizando - Tirando duas funcoes - deixando somente com um tipo de constante - deixando a tela de inicio maior - acrescentando alguns comentarios

--- Jogo.py
+++ Jogo.py
@@ -4,6 +4,7 @@ from pygame.constants import *
 from random import *
 from Classes import *
 from constantes import *
+from Funcoes import *
 import pygame
 import random
 import os
@@ -13,13 +14,6 @@ pygame.mixer.init()
 # Criando a tela do jogo
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Olha o Carteiro!')
-
-# Exibir mensagens:
-def mensagem(msg, tamanho, cor):
-    fonte = pygame.font.Font('font/PressStart2P.ttf', tamanho)
-    mensagem = f"{msg}"  # F--F string--- Mesma coisa que .format
-    texto_formatado = fonte.render(mensagem, True, cor)  # Textocerrilhado- Segundo
-    return texto_formatado
 
 # Carregando imagens:
 background = pygame.image.load('Imagens/8bitNY.jpg').convert()  # Não precisa de transparência aqui
@@ -37,27 +31,6 @@ cone_img = pygame.transform.scale(cone_img, (WIDTH_cone, HEIGHT_cone))
 # Carregando sons:
 som_do_pulo = pygame.mixer.Sound('Som/mb_jump.wav')
 
-
-def iniciando(screen):
-    clock = pygame.time.Clock()
-    background = pygame.image.load('Imagens/8bitNY.jpg').convert()
-    background = pygame.transform.scale(background, (1000, 800))
-    background_rect = background.get_rect()
-    correr = True
-    while correr:
-        clock.tick(FramePerSecond)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                state = DESISTINDO
-                correr = False
-            if event.type == KEYUP:
-                state = JOGANDO
-                correr = False
-        screen.blit(background, background_rect)
-        pygame.display.flip()
-    return state
-
-
 def joguinho(tela):
     # Rodando o cenário
     clock = pygame.time.Clock()
@@ -65,14 +38,9 @@ def joguinho(tela):
     todas_as_sprites = pygame.sprite.Group()
     carteiro_andando = Carteiro(carteiro_sheet)
     todas_as_sprites.add(carteiro_andando)
+    
     # Grupo colisão com a carteira
     grupo_obstaculo = pygame.sprite.Group()
-    
-    ACABOU = 0
-    JOGAR = 1
-    MORTO = 2
-    state = JOGAR
-
     obstaculo = None
     obstaculo_invisivel = None
 
@@ -80,7 +48,10 @@ def joguinho(tela):
     bg_e = 0
     bg_d = background.get_width()
     
-    #pygame.mixer.music.play(loops=-1)    
+    # Música de fundo do jogo
+    #pygame.mixer.music.play(loops=-1)
+    
+    state = JOGAR   
     # Loop Principal!
     while state != ACABOU:
         clock.tick(FramePerSecond)
@@ -99,6 +70,7 @@ def joguinho(tela):
                             pass
                         else:
                             carteiro_andando.pula()
+            # Se colidiu com um obstáculo
             if state == MORTO:
                 if event.type == KEYDOWN:
                     if event.key == K_SPACE:
@@ -109,23 +81,27 @@ def joguinho(tela):
                             colisoes.clear()
                             obstaculo = None
                     elif event.key == QUIT or event.key == K_q:
-                        state = DESISTINDO
+                        state = MORTO
 
+        # Para surgir os obstáculos
         if obstaculo == None or obstaculo.rect.x < (largura - 400):
+            # Aleatoriamente
             opcao = random.randint(1, 2)
+            # Surgirá um cone
             if opcao == 1:
                 obstaculo = Cone(cone_img)
-
+            # Surgirá um poste
             if opcao == 2:
                 referencia = Poste(poste_img)
                 obstaculo = Quadrado(poste_img, referencia)
                 todas_as_sprites.add(referencia)
-
             todas_as_sprites.add(obstaculo)
             grupo_obstaculo.add(obstaculo)
-            
+
+        # Caso colida com algum dos obstáculos, o fundo para de andar    
         if state == MORTO:
-            velocidade_tela = 0            
+            velocidade_tela = 0
+        # Do contrário, o fundo continua a andar e atualizar as sprites
         else:
             velocidade_tela = 2.4
             todas_as_sprites.update()
@@ -137,15 +113,13 @@ def joguinho(tela):
         # Velocidade com que o fundo se mexe
         bg_e -= velocidade_tela
         bg_d -= velocidade_tela
-
         # Quando os fundos saem completamente da tela, eles voltam para o início para passarem novamente
         if bg_e < background.get_width() * -1:
             bg_e = background.get_width()
-
         if bg_d < background.get_width() * -1:
             bg_d = background.get_width()
 
-        # Linhas importantes = Fazem o jogo ficar sempre se atualizando
+        # Linhas importantes = Fazem a tela do jogo ficar sempre se atualizando
         tela.blit(background, (bg_e, -270))
         tela.blit(background, (bg_d, -270))
 
@@ -154,18 +128,20 @@ def joguinho(tela):
             tela.blit(cabou, (70, 100))
             restart = mensagem("Pressione espaço para reiniciar!", 25, (255, 255, 0))
             tela.blit(restart, (70, 180))
-        # Caso colida com algum dos obstáculos, o fundo para de andar
         todas_as_sprites.draw(tela)
+        
+        # Atualizando o jogo
         pygame.display.update()
     return state
 
-state = INICIANDO
-while state != DESISTINDO:
-    if state == INICIANDO:
+state = ACABOU
+while state != MORTO:
+    if state == ACABOU:
         state = iniciando(tela)
-    elif state == JOGANDO:
+    elif state == JOGAR:
         state = joguinho(tela)
     else:
-        state = DESISTINDO
+        state = MORTO
+
 # Encerrando o PyGame
 pygame.quit()

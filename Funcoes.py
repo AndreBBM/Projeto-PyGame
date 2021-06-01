c7c139c97e90097215931f72d1414e44f55779dc
brigidolivia <livia.outreach@gmail.com>
2021-06-01 06:39:32 -0300

    Divisão entre os loops e as funções complementares: -Melhor manuseio para modificar

--- Funcoes.py
+++ Funcoes.py
@@ -9,22 +9,3 @@ def mensagem(msg, tamanho, cor):
     mensagem = f"{msg}"  # F--F string--- Mesma coisa que .format
     texto_formatado = fonte.render(mensagem, True, cor)  # Textocerrilhado- Segundo
     return texto_formatado
-
-def iniciando(screen):
-    clock = pygame.time.Clock()
-    background = pygame.image.load('Imagens/8bitNY.jpg').convert()
-    background = pygame.transform.scale(background, (largura, altura))
-    background_rect = background.get_rect()
-    correr = True
-    while correr:
-        clock.tick(FramePerSecond)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                state = MORTO
-                correr = False
-            if event.type == KEYUP:
-                state = JOGAR
-                correr = False
-        screen.blit(background, background_rect)
-        pygame.display.flip()
-    return state

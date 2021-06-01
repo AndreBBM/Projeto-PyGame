cf51adad55b9e49104c28463d52b95462afc4265
brigidolivia <livia.outreach@gmail.com>
2021-06-01 06:38:23 -0300

    Readicionando Comentários- LoopJogo

--- LoopJogo.py
+++ LoopJogo.py
@@ -3,18 +3,13 @@ from Classes import *
 from constantes import *
 import pygame
 import random
+from Funcoes import*
 
 pygame.init()
 pygame.mixer.init()
 # Criando a tela do jogo
 pygame.display.set_caption('Olha o Carteiro!')
 
-# Exibir mensagens:
-def mensagem(msg, tamanho, cor):
-    fonte = pygame.font.Font('font/PressStart2P.ttf', tamanho)
-    mensagem = f"{msg}"  # F--F string--- Mesma coisa que .format
-    texto_formatado = fonte.render(mensagem, True, cor)  # Textocerrilhado- Segundo
-    return texto_formatado
 # Carregando imagens:
 background = pygame.image.load('Imagens/8bitNY.jpg').convert()  # Não precisa de transparência aqui
 background = pygame.transform.scale(background, (1000, 800))
@@ -53,8 +48,10 @@ def executarjoguinho(tela):
     # São criados 2 fundos, um incial e outro logo após o primeiro, que aparece quando o primeiro sai da tela
     bg_e = 0
     bg_d = background.get_width()
-    
-    #pygame.mixer.music.play(loops=-1)    
+       
+    # Música de fundo do jogo
+    #pygame.mixer.music.play(loops=-1)
+
     # Loop Principal!
     while state != ACABOU:
         clock.tick(FramePerSecond)
@@ -73,6 +70,8 @@ def executarjoguinho(tela):
                             pass
                         else:
                             carteiro_andando.pula()
+
+            # Se colidiu com um obstáculo
             if state == MORTO:
                 if event.type == KEYDOWN:
                     if event.key == K_SPACE:
@@ -85,11 +84,16 @@ def executarjoguinho(tela):
                     elif event.key == QUIT or event.key == K_q:
                         state = MORTO
 
+         # Para surgir os obstáculos
         if obstaculo == None or obstaculo.rect.x < (largura - 400):
+            # Aleatoriamente
             opcao = random.randint(1, 2)
+
+            # Surgirá um cone
             if opcao == 1:
                 obstaculo = Cone(cone_img)
 
+            # Surgirá um poste
             if opcao == 2:
                 referencia = Poste(poste_img)
                 obstaculo = Lampada(poste_img, referencia)
@@ -97,9 +101,12 @@ def executarjoguinho(tela):
 
             todas_as_sprites.add(obstaculo)
             grupo_obstaculo.add(obstaculo)
-            
+
+        # Caso colida com algum dos obstáculos, o fundo para de andar    
         if state == MORTO:
-            velocidade_tela = 0            
+            velocidade_tela = 0 
+
+        # Do contrário, o fundo continua a andar e atualizar as sprites           
         else:
             velocidade_tela = 2.4
             todas_as_sprites.update()
@@ -119,7 +126,7 @@ def executarjoguinho(tela):
         if bg_d < background.get_width() * -1:
             bg_d = background.get_width()
 
-        # Linhas importantes = Fazem o jogo ficar sempre se atualizando
+        # Linhas importantes = Fazem a tela do jogo ficar sempre se atualizando
         tela.blit(background, (bg_e, -270))
         tela.blit(background, (bg_d, -270))
 
@@ -128,7 +135,10 @@ def executarjoguinho(tela):
             tela.blit(cabou, (70, 100))
             restart = mensagem("Pressione espaço para reiniciar!", 25, (255, 255, 0))
             tela.blit(restart, (70, 180))
-        # Caso colida com algum dos obstáculos, o fundo para de andar
+        
         todas_as_sprites.draw(tela)
+
+        # Atualizando o jogo
         pygame.display.update()
+        
     return state

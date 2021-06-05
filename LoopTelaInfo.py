32d2a4fc7e20438a304f0935d504677fe98bd618
GiovanaCA <giovana.gca2002@gmail.com>
2021-06-05 18:59:08 -0300

    duas mudanças - mudando a imagem de fundo utilizada - acrescentando as mensagens sobre o objetivo e como jogar

--- LoopTelaInfo.py
+++ LoopTelaInfo.py
@@ -10,13 +10,13 @@ def executar_tela_info (screen):
     pygame.mixer.music.set_volume(volume)
     clock = pygame.time.Clock()
     # Tela de fundo
-    background = pygame.image.load('Imagens/8bitNY.jpg').convert()
+    background = pygame.image.load('Imagens/carta.jpg').convert()
     background = pygame.transform.scale(background, (largura, altura))
     background_rect = background.get_rect()
 
     correr = True
 
-     # Música de fundo do jogo começa a tocar
+    # Música de fundo do jogo começa a tocar
     pygame.mixer.music.play(loops=-1)
     while correr:
 
@@ -34,7 +34,36 @@ def executar_tela_info (screen):
                     # pygame.mixer.music.stop()
 
         # Atualizando a tela
+        # Imagem de fundo: carta
         screen.blit(background, background_rect)
+
+        # Nome do jogo
+        nome = mensagem("Olha o carteiro!", 35, (0, 0, 0))
+        screen.blit(nome, (190, 50))
+
+        # Mensagem com o objetivo do jogo
+        objetivo1 = mensagem("Objetivo do", 30, (0, 0, 0))
+        screen.blit(objetivo1, (70, 295))
+        objetivo2 = mensagem("jogo:", 30, (0, 0, 0))
+        screen.blit(objetivo2, (165, 335))
+        objetivo3 = mensagem("Desviar dos cones", 20, (0, 0, 0))
+        screen.blit(objetivo3, (70, 375))
+        objetivo4 = mensagem("e das lâmpadas", 20, (0, 0, 0))
+        screen.blit(objetivo4, (100, 405))
+        objetivo4 = mensagem("dos postes.", 20, (0, 0, 0))
+        screen.blit(objetivo4, (130, 435))
+
+        # Mensagem sobre como jogar
+        instrucoes1 = mensagem("Como jogar?", 30, (0, 0, 0))
+        screen.blit(instrucoes1, (520, 300))
+        instrucoes2 = mensagem("Para pular", 20, (0, 0, 0))
+        screen.blit(instrucoes2, (590, 340))
+        instrucoes3 = mensagem("pressione", 20, (0, 0, 0))
+        screen.blit(instrucoes3, (595, 370))
+        instrucoes4 = mensagem("a setinha", 20, (0, 0, 0))
+        screen.blit(instrucoes4, (595, 400))
+        instrucoes4 = mensagem("para cima.", 20, (0, 0, 0))
+        screen.blit(instrucoes4, (595, 430))
 
         pygame.display.flip()
     return state

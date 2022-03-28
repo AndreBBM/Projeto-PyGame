7b929086ccca8de19f0bfd5a559a9170b106b68b
GiovanaCA <giovana.gca2002@gmail.com>
2022-03-28 20:58:31 -0300

    Fazendo alteracoes na tela

--- LoopTelaInfo.py
+++ LoopTelaInfo.py
@@ -3,7 +3,7 @@ from Classes_Funcoes import *
 from constantes import *
 import pygame
 import LoopJogo
-
+from extrair_metodo import *
 
 # Função da segunda tela
 def executar_tela_info(screen):
@@ -45,34 +45,21 @@ def executar_tela_info(screen):
         # Atualizando a tela
         # Imagem de fundo: carta
         screen.blit(background, background_rect)
+        
         # Nome do jogo
-        nome = mensagem("Olha o carteiro!", 35, (0, 0, 0))
+        nome = mensagem("Olha o carteiro!", 35, cor_b)
         screen.blit(nome, (190, 50))
-        # Mensagem com o objetivo do jogo
-        objetivo1 = mensagem("Objetivo", 20, (0, 0, 0))
-        screen.blit(objetivo1, (60, 260))
-        objetivo2 = mensagem("do jogo:", 20, (0, 0, 0))
-        screen.blit(objetivo2, (65, 280))
-        objetivo3 = mensagem("Desviar dos", 15, (0, 0, 0))
-        screen.blit(objetivo3, (60, 330))
-        objetivo4 = mensagem("cones e das", 15, (0, 0, 0))
-        screen.blit(objetivo4, (60, 350))
-        objetivo5 = mensagem("lâmpadas", 15, (0, 0, 0))
-        screen.blit(objetivo5, (80, 370))
-        objetivo6 = mensagem("dos postes.", 15, (0, 0, 0))
-        screen.blit(objetivo6, (60, 390))
-
-        # Mensagem sobre o player
-        msg1 = mensagem("Escolha um player:", 20, (255, 0, 0))
+        
+        # Mensagem "Escolha o jogador"
+        msg1 = mensagem("Escolha um player:", 20, cor_v)
         screen.blit(msg1, (260, 240))
-        msg2 = mensagem("Carteira", 15, (0, 0, 0))
-        screen.blit(msg2, (280, 270))
-        msg3 = mensagem("Carteiro", 15, (0, 0, 0))
-        screen.blit(msg3, (460, 270))
-        msg4 = mensagem("Letra A", 15, (0, 0, 0))
-        screen.blit(msg4, (290, 450))
-        msg5 = mensagem("Letra O", 15, (0, 0, 0))
-        screen.blit(msg5, (470, 450))
+
+        # Mensagens das instruções na tela
+        lista = aparece(m, t)
+        i = 0
+        for x in lista:
+            screen.blit(x, (p[i], p[i+1]))
+            i += 2
 
         # Imagens das duas opções
         carteira_sheet = pygame.image.load('Assets/Imagens/MailmanFemaleSpriteSheet.png').convert_alpha()
@@ -83,18 +70,6 @@ def executar_tela_info(screen):
         carteiro_sheet = carteiro_sheet.subsurface((235, 0), (235, 336))
         carteiro_sheet = pygame.transform.scale(carteiro_sheet, (120, 170))
         screen.blit(carteiro_sheet, (470, 280))
-
-        # Mensagem sobre como jogar
-        instrucoes1 = mensagem("Como jogar?", 20, (0, 0, 0))
-        screen.blit(instrucoes1, (630, 265))
-        instrucoes2 = mensagem("Para pular", 15, (0, 0, 0))
-        screen.blit(instrucoes2, (670, 320))
-        instrucoes3 = mensagem("pressione", 15, (0, 0, 0))
-        screen.blit(instrucoes3, (675, 340))
-        instrucoes4 = mensagem("a setinha", 15, (0, 0, 0))
-        screen.blit(instrucoes4, (675, 360))
-        instrucoes4 = mensagem("para cima.", 15, (0, 0, 0))
-        screen.blit(instrucoes4, (670, 380))
 
         pygame.display.flip()
     return state, player
